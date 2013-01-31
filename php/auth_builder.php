af37110f60119c957dba29c2dea70b5d341a6c73
rojaster <rojaster@yandex.ru>
2013-01-31 18:31:38 +0600

    simple connecter, oop expirements

--- php/auth_builder.php
+++ php/auth_builder.php
@@ -7,4 +7,11 @@
 *if data auth is good then user is           *
 *redirected to main page                     *
 ********************************************/
+require_once("connecter.php");
+
+
+$DB = new Connecter();
+
+if($DB == 0) {echo("DAMN SHIT ");} else {echo("good shit");}
+
 
--- php/connecter.php
+++ php/connecter.php
@@ -11,49 +11,22 @@ Class Connecter{
 		private $dbname = 'usctestdb';	// database name
 		private $dblink;				// variable for database link
 
-		function Connecter(){
-				$this->dblink = mysql_connect($this->host,$this->dbuser,$this->dbpassw) 
-									or die("<br/>we don't connected to dtabase , check connection config");//bad news for us , we don't connected to database, may be dblink was dropped and we must create it again
-				echo("<br/>database link was created");
-				return $this->dblink;
+		function __construct(){
+				$this->dblink = @mysql_connect($this->host,$this->dbuser,$this->dbpassw) 
+											or die("Can't connect to database, check parameters"); // fail 
 		}
-
-		function createDB(){
-				if(!$this->dblink) die("<br/>dblink is not a pointer to DB, check it"); // if value equals is 0 - connection is fault, 1 - good  and DB was dropped
-				if(mysql_select_db($this->dbname,$this->dblink)) die("can't select db, check it"); // db was created early, why u must do it again?
-				$sql = "CREATE DATABASE {$this->dbname}";
-				mysql_query($sql) or die("<br/>WRONG!!!CREATE DATABASE....."); // query was crashed, and you should view at log file, something wrong with query to db, check it
-				$sql = "SET NAMES utf8";
-				mysql_query($sql) or die("<br/>WRONG!!!SET NAMES utf8"); // query was crashed, and you should view at log file, something wrong with query to db, check it
-				echo("<br/>DB was created, push @create tables@ to create a tables in db");
+		
+		function __destruct(){
+				$res = @mysql_close($this->dblink) 
+											or die("Can't close connection to current link"); // close connection 
 		}
+		
 
-		function createTB(){
-			/**/
-		}
-
-		function dropTB(){
-			/**/
-		}
-
-		function dropDB(){
-				if(!mysql_select_db($this->dbname,$this->dblink)) die("<br/>database already was dropped , check it for dropDB()"); // database already was  dropped  
-				$sql = "DROP DATABASE {$this->dbname}";
-				if(!$this->dblink) die("<br/>WRONG!!!dblink is not a pointer to DB"); // if value equals is 0 - connection is fault, 1 - good  and DB was dropped
-				mysql_query($sql,$this->dblink) or die("<br/>WRONG!!! query was crashed, {".mysql_error()."}"); // query was crashed, and you should view at log file 
-				echo('<br/>DATABASE WAS DROPPED');
+		function query($qstr){
+				$qstr = mysql_real_escape_string(addslashes($qstr));
 		}
 
 
 }
-$DB = new Connecter();
-if(isset($_POST['createdb'])){$DB->createDB();}
-if(isset($_POST['dropdb'])){ $DB->dropDB();}
-
 
 ?>
-<br/>
-<form action="<?=$_SERVER['PHP_SELF']?>" method="POST" name="frm">
-	<input type="submit" value="createdb" name="createdb"><br/>
-	<input type="submit" value="dropdb" name="dropdb">
-</form>
