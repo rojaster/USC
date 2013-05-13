6884e922f552d5a4b84f40564fc5ec5b71a07526
rojaster <rojaster@yandex.ru>
2013-05-13 22:19:36 +0600

    + add globals constants (globals.php)

--- php/globals.php
+++ php/globals.php
@@ -18,3 +18,15 @@ define('__DBUSER__','root',false);			//database user
 define('__DBPASS__','',false);				// password for database connection
 define('__DBHOST__','localhost',false);		// host, where database is 
 define('__DBNAME__','usctestdb',false);		// database name on server
+
+
+/*NAMES OF CATEGORIES*/
+define('__SIM__','Симкарты',false);
+define('__DEVICES__','Приборы',false);
+define('__SENSORS__','Датчики',false);
+define('__AUTOS__','Машины',false);
+define('__MONTAGE__','Монтаж',false);
+define('__TSERVICE__','Тех.Обслуживание',false);
+define('__WORKERS__','Служащие',false);
+define('__CLIENTS__','Клиенты',false);
+define('__STATISTICS__','Статистика',false);
